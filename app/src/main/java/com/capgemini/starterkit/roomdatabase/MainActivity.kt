5498a546979137f3e0a56c389bdee208eb2c5794
vaalewad <vaishnaviaalewad@gmail.com>
2023-09-20 19:37:31 +0530

    Made changes in the variable and file names

--- app/src/main/java/com/capgemini/starterkit/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/capgemini/starterkit/roomdatabase/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : ComponentActivity() {
     }
     private val userAdapter = UserAdapter()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
