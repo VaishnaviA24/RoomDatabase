c579573aa1290466ee162605bd9af672b81c7cba
vaalewad <vaishnaviaalewad@gmail.com>
2023-09-21 16:19:54 +0530

    pushing the changes since the previous push failed

--- app/src/main/java/com/capgemini/starterkit/roomdatabase/viewmodel/EmployeeViewModel.kt
+++ app/src/main/java/com/capgemini/starterkit/roomdatabase/viewmodel/EmployeeViewModel.kt
@@ -15,9 +15,7 @@ class EmployeeViewModel(
     // Creates a LiveData that has values collected from the origin Flow.
     val getAllData: LiveData<List<EmployeeEntity>> = repository.allUsersData.asLiveData()
 
-    /**
-     * Launching a new coroutine to insert the data in a non-blocking way
-     */
+    //Launching a new coroutine to insert the data in a non-blocking way
     fun insertData(employeeEntityInfo: EmployeeEntity) = viewModelScope.launch {
         repository.insertData(employeeEntityInfo)
     }
